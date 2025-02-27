3e617b6aacf24524563e4bf684dc72263ff24288
Emman Oduor <wyllademanu003@gmail.com>
2025-02-27 16:47:16 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//    console.log(`I'm ${22} years old. Happy birthday to me!`);
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+function writeCards(names, event) {
+    let thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+        number -= 1;
+    }
+}
